9a3236f9421f40f1e98abe3bb1126cc739d29659
Erczzz <erczbarrientos1@gmail.com>
2023-03-29 14:13:58 +0800

    Added JRSDBContext

---
+++ JewelryRentalSystem/Data/JRSDBContext.cs
@@ -0,0 +1,23 @@
+﻿using JewelryRentalSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JewelryRentalSystem.Data
+{
+    public class JRSDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string con = "Server = (localdb)\\MSSQLLocalDB; Database = JRSDB; Integrated Security = true;";
+            optionsBuilder.UseSqlServer(con).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<AppointmentType> AppointmentTypes { get; set;}
+        public DbSet<Payment> Payments { get; set; }
+    }
+}
