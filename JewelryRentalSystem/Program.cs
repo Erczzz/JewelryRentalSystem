3ff7786f5c0dcb03838549e82d9d424d20d32e20
Erczzz <erczbarrientos1@gmail.com>
2023-03-30 14:32:13 +0800

    Changing homepage

--- JewelryRentalSystem/Program.cs
+++ JewelryRentalSystem/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Product}/{action=GetAllProducts}/{id?}");
 
 app.Run();
