7c46a7eeee27c0dad626be016d43cea5e96a1eab
Erczzz <erczbarrientos1@gmail.com>
2023-04-05 16:04:11 +0800

    Added logo

---
+++ JewelryRentalSystem/Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JewelryRentalSystem.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int CartId { get; set; }
+        public int CustomerId { get; set; }
+        public int ProductName { get; set; }
+        public double ProductPrice { get; set; }
+        public double ProductQty { get; set; }
+        public double TransactionTotal { get; set; }
+    }
+}
